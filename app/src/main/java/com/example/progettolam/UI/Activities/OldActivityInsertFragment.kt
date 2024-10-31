e3dc5ee5f28abbb6a47197f40799d4c7fb9cf53d
Ale <118299985+AleBombi@users.noreply.github.com>
2024-10-31 21:27:22 +0100

    AV

--- app/src/main/java/com/example/progettolam/UI/Activities/OldActivityInsertFragment.kt
+++ app/src/main/java/com/example/progettolam/UI/Activities/OldActivityInsertFragment.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,21 @@ import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.example.progettolam.DB.ActivityRepository
+import com.example.progettolam.DB.ActivityString
+import com.example.progettolam.DB.ActivityType
+import com.example.progettolam.DB.ActivityViewModel
+import com.example.progettolam.DB.ActivityViewModelFactory
+import com.example.progettolam.DB.BaseActivity
+import com.example.progettolam.DB.RunningActivity
+import com.example.progettolam.DB.WalkingActivity
 import com.example.progettolam.R
+import com.example.progettolam.UI.homeFragment.HomeViewModel
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 
 class OldActivityInsertFragment: Fragment() {
@@ -31,6 +46,10 @@ class OldActivityInsertFragment: Fragment() {
     private var mHour: Int = 0
     private var mMinute: Int = 0
 
+    private val activityViewModel by lazy {
+        val factory = ActivityViewModelFactory(ActivityRepository(requireActivity().application))
+        ViewModelProvider(this, factory)[ActivityViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,12 +83,14 @@ class OldActivityInsertFragment: Fragment() {
         btnDatePicker.setOnClickListener{
             chooseDate()
         }
-
         btnStartTimePicker.setOnClickListener{
             chooseTime(txtStartTime)
         }
         btnEndTimePicker.setOnClickListener{
             chooseTime(txtEndTime)
+        }
+        saveBtn.setOnClickListener{
+            saveNewActivity()
         }
     }
 
@@ -107,5 +128,109 @@ class OldActivityInsertFragment: Fragment() {
             false
         )
         timePickerDialog.show()
+    }
+
+    private fun getTypeActivity(): String {
+        return spinnerActivity.selectedItem.toString()
+    }
+
+    private fun getDate(): String {
+        return txtDate.text.toString()
+    }
+
+    private fun getStartTime(): String {
+        return txtStartTime.text.toString()
+    }
+
+    private fun getEndTime(): String {
+        return txtEndTime.text.toString()
+    }
+
+    private fun isIntervalTimeCorrect(): Boolean {
+        var isCorrectTime : Boolean = false
+        try {
+            val startTime = getLocalTimeFromString(txtStartTime)
+            val endTime = getLocalTimeFromString(txtEndTime)
+
+            // Check if start time is before end time
+            if (startTime != null) {
+                isCorrectTime = startTime.isBefore(endTime)
+            }
+        } catch (e: Exception) {
+            isCorrectTime = false
+        }
+        return isCorrectTime
+    }
+
+    private fun getLocalTimeFromString(textView: TextView): LocalTime? {
+        val timeFormatter = DateTimeFormatter.ofPattern("H:mm")
+        var localTime: LocalTime? = null
+        try {
+            localTime = LocalTime.parse(textView.text.toString(), timeFormatter)
+        } catch (e: Exception) {
+            // Handle parsing error (e.g., if the times are not set correctly)
+            Log.i("LocalTime", "Error")
+        }
+        return localTime
+    }
+
+    private fun getLocalDateFromString(textView: TextView): LocalDate? {
+        val dateFormatter = DateTimeFormatter.ofPattern("d-M-yyyy")
+        var localDate: LocalDate? = null
+        try {
+            localDate = LocalDate.parse(textView.text, dateFormatter)
+        } catch (e: Exception) {
+            // Handle parsing error (e.g., if the times are not set correctly)
+            Log.i("LocalTime", "Error")
+        }
+        return localDate
+    }
+
+    private fun saveNewActivity() {
+        val typeActivity = getTypeActivity()
+        var a = getString(R.string.run_tag)
+        if (typeActivity != "" && getDate() != "" && getStartTime() != "" && getEndTime() != "" /*&& TODO: controllare isIntervalTimeCorrect()*/) {
+            val startTime = getLocalTimeFromString(txtStartTime)
+            val endTime = getLocalTimeFromString(txtEndTime)
+            val date = getLocalDateFromString(txtDate)
+
+            when (typeActivity) {
+                getString(R.string.walk_tag) -> {
+                    activityViewModel.insertWalkingActivity(
+                        BaseActivity(
+                            null,
+                            ActivityType.RUNNING,
+                            startTime,
+                            date,
+                            endTime,
+                            date
+                        ),
+                        WalkingActivity(null, null, 0f)
+                    )
+                }
+
+                getString(R.string.run_tag) -> {
+                    activityViewModel.insertRunningActivity(
+                        BaseActivity(
+                            null,
+                            ActivityType.RUNNING,
+                            startTime,
+                            date,
+                            endTime,
+                            date
+                        ),
+                        RunningActivity(null, null, 0f)
+                    )
+                }
+
+                getString(R.string.chilling_tag) -> {
+                    //TODO: Insert Chilling Activity
+                }
+
+                getString(R.string.drive_tag) -> {
+                    // TODO: Insert Driving Activity
+                }
+            }
+        }
     }
 }
