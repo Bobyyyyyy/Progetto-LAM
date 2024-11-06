b8251ac400d59188e09a06b02b865d86d5873033
Ale <118299985+AleBombi@users.noreply.github.com>
2024-11-06 15:11:31 +0100

    fix layout

--- app/src/main/java/com/example/progettolam/UI/geofenceFragment/MyReceiver.kt
+++ app/src/main/java/com/example/progettolam/UI/geofenceFragment/MyReceiver.kt
@@ -9,7 +9,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.progettolam.MainActivity
-import com.example.progettolam.MapsActivity
 import com.example.progettolam.R
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
